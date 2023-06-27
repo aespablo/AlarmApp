a2504850d0153aa5d48c8c9e6daeb3d18f675ad4
Pablo <pablo@stelsi.org>
2023-06-27 18:31:45 +0900

    For Android Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,34 @@
+import 'dart:isolate';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:alarm/src/home_page.dart';
+import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
+import 'package:logger/logger.dart';
+
+// Be sure to annotate your callback function to avoid issues in release mode on Flutter >= 3.3.0
+@pragma('vm:entry-point')
+void printHello() {
+  final DateTime now = DateTime.now();
+  final int isolateId = Isolate.current.hashCode;
+  Logger().v("[$now] Hello, world! isolate=$isolateId function='$printHello'");
+}
 
 Future<void> main() async {
+  // Be sure to add this line if initialize() call happens before runApp()
   final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await AndroidAlarmManager.initialize();
   await Future.delayed(const Duration(milliseconds: 10));
   runApp(const ProviderScope(child: MyApp()));
+  const int helloAlarmID = 0;
+  await AndroidAlarmManager.periodic(
+    const Duration(seconds: 10),
+    helloAlarmID,
+    printHello,
+  );
   FlutterNativeSplash.remove();
 }
 
